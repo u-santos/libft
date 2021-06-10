2b6fe8b5a0c41f51ce3387dba71d271830a1809c
Ulisses Oliveira <ulissees.oliveira@gmail.com>
2021-06-10 01:29:17 -0300

    Fixed

--- ft_strchr.c
+++ ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: usantos- <usantos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/05 12:28:00 by usantos-          #+#    #+#             */
-/*   Updated: 2021/06/10 01:27:29 by usantos-         ###   ########.fr       */
+/*   Updated: 2021/06/10 01:29:11 by usantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@ char	*ft_strchr(const char *s, int c)
 {
 	if (!s)
 		return (NULL);
+	if (c == 0)
+	{
+		while(*s)
+			s++;
+		return (s);
+	}
 	while (*s)
 	{	
 		if (c == *s)

41c7850edbf68aa9cb2a7dd24af126720a49393d
Ulisses Oliveira <ulissees.oliveira@gmail.com>
2021-06-10 01:04:49 -0300

    Try to fix

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: usantos- <usantos-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 21:56:04 by usantos-          #+#    #+#             */
-/*   Updated: 2021/06/10 00:56:37 by usantos-         ###   ########.fr       */
+/*   Updated: 2021/06/10 01:04:00 by usantos-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,12 +33,12 @@ size_t	ft_just_count_it(const char *str, char c)
 	return (counter);
 }
 
-void	freet(char **array, size_t jndex)
+void	freet(char **array)
 {
 	size_t	index;
 
 	index = 0;
-	while (index < jndex)
+	while (array[index])
 	{
 		free(array[index]);
 		index++;
@@ -61,13 +61,12 @@ char	**ft_splinter(char const *s, char c, char **array)
 			start = index;
 			while (s[index] != c && s[index] != 0)
 				index++;
-			array[jndex] = (char *)ft_calloc(sizeof(char), (index - start + 1));
+			array[jndex] = ft_substr(s, start, (index - start));
 			if (array == 0)
 			{
-				freet(array, jndex);
+				freet(array);
 				return (NULL);
 			}
-			array[jndex] = ft_substr(s, start, (index - start));
 			jndex++;
 		}
 		else if (s[index] != 0)
